6a05b7e78f79fe8c24fa199a0dc765b4c17ac834
nicolasfilangeri <nicolas.filangeri@live.fr>
2021-08-13 00:33:20 +0200

    netflix clone 1.00

---
+++ netflix/src/Nav.js
@@ -0,0 +1,35 @@
+import "./Nav.css";
+import React, { useEffect, useState } from "react";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png-1200x681.png"
+        alt-="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
